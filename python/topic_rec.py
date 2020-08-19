118af5ae159a6399d30c0e4d5e3e6a8f7cc56e5d
Yoav Dobrin <37622785+yodobrin@users.noreply.github.com>
2020-08-19 11:49:56 +0300

    Update topic_rec.py

--- python/topic_rec.py
+++ python/topic_rec.py
@@ -1,33 +1,28 @@
+# ------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# -------------------------------------------------------------------------
 
 
 
-
-# pylint: disable=C0111
-
+import sys
 import os
-import asyncio
-from azure.servicebus.aio import ServiceBusClient
+
+from azure.servicebus import SubscriptionClient
 
 
+connection_str = os.environ['SERVICE_BUS_CONNECTION_STR']
 
-CONNECTION_STR = os.environ['SERVICE_BUS_CONNECTION_STR']
-TOPIC_NAME = os.environ["SERVICE_BUS_TOPIC_NAME"]
-SUBSCRIPTION_NAME = os.environ["SERVICE_BUS_SUBSCRIPTION_NAME"]
+if __name__ == '__main__':
 
-async def main():
-    servicebus_client = ServiceBusClient.from_connection_string(conn_str=CONNECTION_STR)
+    sub_client = SubscriptionClient.from_connection_string(
+        connection_str, name="pytopic/Subscriptions/pysub", debug=False)
 
-    async with servicebus_client:
-        receiver = servicebus_client.get_subscription_receiver(
-            topic_name=TOPIC_NAME,
-            subscription_name=SUBSCRIPTION_NAME
-        )
-        async with receiver:
-            received_msgs = await receiver.receive_messages(max_batch_size=2, max_wait_time=5)
-            for msg in received_msgs:
-                print(str(msg))
-                await msg.complete()
-    
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+    with sub_client.get_receiver() as receiver:
+        batch = receiver.fetch_next(timeout=10)
+        while batch:
+            print("Received {} messages".format(len(batch)))
+            for message in batch:
+                message.complete()
+            batch = receiver.fetch_next(timeout=10)
